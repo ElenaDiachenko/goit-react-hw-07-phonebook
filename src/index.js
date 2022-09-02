586452547804eaae1719b6ea96e41e4ddf7b04b5
ElenaDiachenko <96205320+ElenaDiachenko@users.noreply.github.com>
2022-09-02 23:11:37 +0300

    add dep

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 import { GlobalStyle } from './components/GlobalStyle';
+import store from './redux/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
